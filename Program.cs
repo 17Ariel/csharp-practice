d8391edd3cac20ea4ee415dc143ca27b5228eee9
Vang Hou <vangholou@gmail.com>
2022-08-27 16:05:36 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace basics
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Console.WriteLine("hellow world");
+
+
+            // accept user input and print
+            // string name;
+
+            // Console.Write("Enter your name: ");
+            // name = Console.ReadLine();
+
+            // string yourName = name;
+
+            // Console.WriteLine("Your name is: " + yourName);
+
+            // variables
+
+            // int age = 23;
+            // double change = 34.900;
+            // float change2 = 90.8F;
+            // long cash = 190000000;
+            // bool isActive = true;
+            // string fullname = "John Doe";
+            // char letter = 'e';
+
+            // Console.WriteLine("Im " + fullname + "My salary is " + cash + ", my favorite letter is " + letter + "My age is " + age + "my change in pocket is" + change2 + "and in the bag is" + change + "and im currently" + isActive + "in facebook");
+
+            //operators
+
+            int numb1 = 10, numb2 = 9;
+
+            Console.WriteLine(numb1 + numb2);
+            Console.WriteLine(numb1 - numb2);
+            Console.WriteLine(numb1 * numb2);
+            Console.WriteLine(numb1 / numb2);
+            Console.WriteLine(numb1 % numb2);
+
+
+
+
+        }
+    }
+}
